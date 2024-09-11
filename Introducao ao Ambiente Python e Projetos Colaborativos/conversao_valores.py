9ccd20bf732b7265795f669606ed2e24044f619d
Weverton Rocha <wevertonrocha@yahoo.com>
2024-09-11 16:41:46 -0300

    introducao python

---
+++ Introducao ao Ambiente Python e Projetos Colaborativos/conversao_valores.py
@@ -0,0 +1,62 @@
+# Inteiro p/ float
+preco = 10
+print(preco)
+print(type(preco))
+#10
+preco = float(preco)
+print(preco)
+print(type(preco))
+#10.0
+preco = 10 / 2
+print(preco)
+print(type(preco))
+
+
+#Float p/ inteiro
+preco = 10.3
+print(preco)
+print(type(preco))
+
+preco = int(preco)
+print(preco)
+print(type(preco))
+
+
+#Conversão por divisão
+preco = 10
+print(preco)
+print(type(preco))
+
+print(preco / 2)
+print(type(preco))
+
+print(preco // 2)
+print(type(preco))
+
+#Numérico p/ string
+
+preco = 10.50
+idade = 28
+print(type(preco))
+print(type(idade))
+
+print(str(idade))
+print(type(idade))
+
+#28
+
+print(str(preco))
+print(type(preco))
+
+#10.50
+
+texto = f"idade {idade} preço {preco}"
+
+print(texto)
+
+#string p/ numero
+print(float(preco))
+print(type(preco))
+
+print(int(idade))
+print(type(idade))
---
+++ Introducao ao Ambiente Python e Projetos Colaborativos/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Weverton"
+idade = 26
+
+nome, idade = "Jessica", 25
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "MG", "RS", "MT"]
+
+print(BRAZILIAN_STATES)
